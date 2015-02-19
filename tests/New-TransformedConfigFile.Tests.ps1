9e5eecd0f5d756497f98061d38d09b49e70c8c41
Lloyd Holman <lloyd.holman@datumgenerics.net>
2015-02-19 18:31:21 +0000

    33 - Completed implementation of New-TransformedConfigFile with happy flow and basic exception handling.

---
+++ tests/New-TransformedConfigFile.Tests.ps1
@@ -0,0 +1,83 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".").Replace(".ps1","")
+$baseModulePath = "$here\..\tools\powershell\modules"
+
+$module = Get-Module $sut
+if ($module -ne $null)
+{
+	Remove-Module $sut
+}
+
+Describe "New-TransformedConfigFile Transform successfully" {
+	Context "When there is a valid source and transform file"  {
+		
+		Import-Module "$baseModulePath\$sut"
+		New-Item -Name "source.xml" -Path $TestDrive -ItemType File
+		New-Item -Name "transform.xslt" -Path $TestDrive -ItemType File
+		New-Item -Name "_TransformedConfigs" -Path $TestDrive -ItemType Directory	
+		$testBasePath = "$TestDrive"		
+		
+		Set-Content $testBasePath\source.xml "<?xml version=""1.0""?><configuration><custom><groups><group name=""TestGroup1""><values><value key=""Test1"" value=""True"" /><value key=""Test2"" value=""600"" /></values></group><group name=""TestGroup2""><values><value key=""Test3"" value=""True"" /></values></group></groups></custom></configuration>"
+		
+		Set-Content $testBasePath\transform.xslt "<?xml version=""1.0""?><configuration xmlns:xdt=""http://schemas.microsoft.com/XML-Document-Transform""><custom><groups><group name=""TestGroup1""><values><value key=""Test2"" value=""601"" xdt:Transform=""Replace"" xdt:Locator=""Match(key)"" /></values></group></groups></custom></configuration>"
+		
+		$expectedOutputFile = "<?xml version=""1.0""?><configuration><custom><groups><group name=""TestGroup1""><values><value key=""Test1"" value=""True"" /><value key=""Test2"" value=""601"" /></values></group><group name=""TestGroup2""><values><value key=""Test3"" value=""True"" /></values></group></groups></custom></configuration>"
+		
+		try {
+			New-TransformedConfigFile -sourceFile "$testBasePath\source.xml" -transformFile "$testBasePath\transform.xslt" -outputFile "$testBasePath\_TransformedConfigs\output.xml" -verbose
+		}
+		catch {
+			throw $_
+		}
+		finally {
+			Remove-Module $sut
+		}
+
+		It "Should generate a valid output XML file" {
+			#Flatten the transformed XML file, removing white space and carriage returns
+			$actualOutputFile = Get-Content "$testBasePath\_TransformedConfigs\output.xml" | Foreach {$_.Trim()} | Out-String
+			$actualOutputFile = $actualOutputFile -replace "`t|`n|`r",""
+			$actualOutputFile | Should BeExactly $expectedOutputFile
+        }
+	}
+}
+
+Describe "New-TransformedConfigFile Transform error" {
+	Context "When there is an invalid source file"  {
+		
+		Import-Module "$baseModulePath\$sut"
+		New-Item -Name "source.xml" -Path $TestDrive -ItemType File
+		New-Item -Name "transform.xslt" -Path $TestDrive -ItemType File
+		$testBasePath = "$TestDrive"		
+		
+		$invalidXmlElement = "conuration"
+		Set-Content $testBasePath\source.xml "<?xml version=""1.0""?><$invalidXmlElement><custom><groups><group name=""TestGroup1""><values><value key=""Test1"" value=""True"" /><value key=""Test2"" value=""600"" /></values></group><group name=""TestGroup2""><values><value key=""Test3"" value=""True"" /></values></group></groups></custom></configuration>"
+		
+		Set-Content $testBasePath\transform.xslt "<?xml version=""1.0""?><configuration xmlns:xdt=""http://schemas.microsoft.com/XML-Document-Transform""><custom><groups><group name=""TestGroup1""><values><value key=""Test2"" value=""601"" xdt:Transform=""Replace"" xdt:Locator=""Match(key)"" /></values></group></groups></custom></configuration>"
+		
+		try {
+			$result = New-TransformedConfigFile -sourceFile "$testBasePath\source.xml" -transformFile "$testBasePath\transform.xslt" -outputFile "$testBasePath\output.xml" -verbose
+		}
+		catch {
+			$result = "$_"
+		}
+		finally {
+			Remove-Module $sut
+		}
+
+		It "Exits the module with a descriptive terminating error" {
+			$expectedErrorMessage = "Whilst executing ctt.exe for sourceFile: $testBasePath\source.xml and transformFile: $testBasePath\transform.xslt, ctt.exe exited with error message: Exception while transforming: System.Xml.XmlException: The 'conuration' start tag on line 1 position 23 does not match the end tag of 'configuration'. Line 1, position 271" -replace "\\","\\"			
+			$result | Should Match $expectedErrorMessage
+		}
+	}
+	
+}
+
+
+$module = Get-Module $sut
+if ($module -ne $null)
+{
+	Remove-Module $sut
+}
+
+
---
+++ tools/powershell/modules/New-TransformedConfigFile.psm1
@@ -0,0 +1,160 @@
+function New-TransformedConfigFile{
+<#
+ 
+.SYNOPSIS
+    Generates a new config transformation file given a source XML file path and XSLT transform file path.
+.DESCRIPTION
+    Generates a new config transformation file given a source XML file path and XSLT transform file path.
+.PARAMETER sourceFile
+	Required.  The full file path to the source XML file to be be transformed.
+.PARAMETER transformFile
+	Required.  The full file path to the XSLT transform file to be be used for transformation.	
+.PARAMETER outputFile
+	Optional.  The full file path to the resultant transformed XML output file.	
+.PARAMETER cttPath
+	Optional.  The full path to the ctt.exe command line executable. Defaults to '\packages\ConfigTransformationTool\tools\ctt.exe', i.e. OneBuilds ConfigTransformationTool NuGet dependency package. 
+.EXAMPLE 
+	Import-Module New-TransformedConfigFile
+	Import the module
+.EXAMPLE	
+	Get-Command -Module New-TransformedConfigFile
+	List available functions
+.EXAMPLE
+	New-TransformedConfigFile -basePath AssemblyInfo.cs
+	Execute the module
+#>
+	[cmdletbinding()]
+		Param(
+			[Parameter(Mandatory = $True,
+						HelpMessage="Please supply a full path")]
+				[ValidateNotNullOrEmpty()]
+				[string]
+				$sourceFile,
+			[Parameter(Mandatory = $True,
+						HelpMessage="Please supply a full path")]
+				[ValidateNotNullOrEmpty()]
+				[string]
+				$transformFile,	
+			[Parameter(Mandatory = $False )]
+				[string]$outputFile = "",					
+			[Parameter(Mandatory = $False )]
+				[string]$cttPath				
+			)
+	Begin {
+			$DebugPreference = "Continue"
+			if (-not $PSBoundParameters.ContainsKey('Verbose'))
+			{
+				$VerbosePreference = $PSCmdlet.GetVariableValue('VerbosePreference')
+			}
+		}	
+	Process {
+				Try {
+						Write-Verbose "New-TransformedConfigFile: Starting: New-TransformedConfigFile"
+						$basePath = Confirm-Path -path ""
+
+						if (!(Test-Path -LiteralPath "$sourceFile"))
+						{
+							throw "Source file: $sourceFile does not exist."
+							
+						}
+						if (!(Test-Path -LiteralPath "$transformFile"))
+						{
+							throw "Transform file: $transformFile does not exist."
+						}					
+						
+						$transformedFilesFolder = "_TransformedConfigs"
+						
+						if ($outputFile -eq "")
+						{
+							#Set the path to the calling scripts path (using Resolve-Path .)
+							$outputFile = "$basePath\$transformedFilesFolder\Output.xml"
+						}
+
+						Write-Verbose "New-TransformedConfigFile: Using source XML file: $sourceFile"
+						Write-Verbose "New-TransformedConfigFile: Using XSLT transform file: $transformFile"
+						Write-Verbose "New-TransformedConfigFile: Setting transformed output XML file to: $outputFile"
+						
+						if ($cttPath -eq "")
+						{
+							$cttPath = Confirm-Path -path "C:\Development\github\OneBuild\lib\ctt.exe"
+							Write-Verbose "New-TransformedConfigFile: 'ctt.exe' command line path set to: $cttPath"
+						}
+
+
+						if (Test-Path $outputFile ) { 
+							Write-Verbose "New-TransformedConfigFile: Deleting existing transform: $ouputFile"
+							Remove-Item -force $outputFile -ErrorAction SilentlyContinue 
+						}
+						
+
+						$result = $null
+						$result = Invoke-ConfigTransformationTool -sourceFile $sourceFile -transformFile $transformFile -outputFile $outputFile -cttPath $cttPath
+						
+						if ($result -ne $null) 
+						{
+							throw "Whilst executing ctt.exe for sourceFile: $sourceFile and transformFile: $transformFile, ctt.exe exited with error message: $result"
+						}
+				} 
+				catch [Exception] {
+					throw "An error occurred transforming sourceFile: $sourceFile. `r`n $_"
+				}
+				
+			}
+}
+
+function Confirm-Path {
+	Param(			
+			[Parameter(
+				Mandatory = $False )]
+				[string]$path			
+		)	
+	Import-Module "$PSScriptRoot\Get-Path.psm1"
+	Try {
+		$path = Get-Path -path $path
+		return $path
+	}
+	Catch {
+		throw $_
+	}
+	Finally {
+		Remove-Module Get-Path
+	}
+}
+
+function Invoke-ConfigTransformationTool {
+	Param(			
+		[Parameter(Mandatory = $True )]
+			[string]$sourceFile,	
+		[Parameter(Mandatory = $True )]
+			[string]$transformFile,	
+		[Parameter(Mandatory = $True )]
+			[string]$outputFile,
+		[Parameter(Mandatory = $True )]
+			[string]$cttPath		
+	)
+	
+	<#								
+	source:{file} (s:) - source file path
+	transform:{file} (t:) - transform file path
+	destination:{file} (d:) - destination file path
+
+	E.g.: ctt.exe source:"source.config" transform:"transform.config" destination:"destination.config"
+	#>	
+
+	#Write-Verbose "New-TransformedConfigFile: & $cttPath source:""$sourceFile"" transform:""$transformFile"" destination:""$outputFile""" 
+	$output = & $cttPath source:"$sourceFile" transform:"$transformFile" destination:"$outputFile" indent verbose 2>&1 
+	$err = $output | ? {$_.gettype().Name -eq "ErrorRecord"}
+	
+	if ($err)
+	{
+		return $err
+	}
+
+	#As we've re-directed error output to standard output (stdout) using '2>&1', so that we can save it to a variable, we have effectively suppressed stdout, therefore we write $output to the Verbose stream here. 
+	$VerbosePreference = "Continue"
+	Write-Verbose ($output | Out-String)
+	return
+
+}
+
+Export-ModuleMember -Function New-TransformedConfigFile
