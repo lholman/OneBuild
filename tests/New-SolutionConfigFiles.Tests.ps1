ed5d413715a482b0fb3ca68ddaaf34020441ae50
Lloyd Holman <lloyd.holman@datumgenerics.net>
2015-02-20 07:01:37 +0000

    #33 - Boiler plate code for New-SolutionConfigFiles added

---
+++ tests/New-SolutionConfigFiles.Tests.ps1
@@ -0,0 +1,42 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".").Replace(".ps1","")
+$baseModulePath = "$here\..\tools\powershell\modules"
+
+$module = Get-Module $sut
+if ($module -ne $null)
+{
+	Remove-Module $sut
+}
+
+Describe "New-SolutionConfigFiles" {
+
+	Context "When there is a [config] folder" {
+	
+		Import-Module "$baseModulePath\$sut"
+		Mock -ModuleName $sut Get-ChildConfigFolders { return "solution.sln"}
+
+		try {
+			New-SolutionConfigFiles
+		}
+		catch {
+			throw
+		}
+		finally {
+			Remove-Module $sut
+		}
+		
+		It "Should call Get-ChildConfigFolders to identify all [config] folders under path" {
+            Assert-MockCalled Get-ChildConfigFolders -ModuleName $sut -Times 1
+        }
+
+	}
+	
+}
+
+$module = Get-Module $sut
+if ($module -ne $null)
+{
+	Remove-Module $sut
+}
+
+
---
+++ tools/powershell/modules/New-SolutionConfigFiles.psm1
@@ -0,0 +1,75 @@
+function New-SolutionConfigFiles{
+<#
+ 
+.SYNOPSIS
+    Identifies all [config] paths under the given solution path.
+.DESCRIPTION
+    Identifies all [config] paths under the given solution path
+.PARAMETER path
+	Optional. The full path to the Visual Studio Solution parent folder to look for [config] folders in.  Defaults to the calling scripts path.	
+.EXAMPLE 
+	Import-Module New-SolutionConfigFiles
+	Import the module
+.EXAMPLE	
+	Get-Command -Module New-SolutionConfigFiles
+	List available functions
+.EXAMPLE
+	New-SolutionConfigFiles -basePath AssemblyInfo.cs
+	Execute the module
+#>
+	[cmdletbinding()]
+		Param(
+			[Parameter(Mandatory = $False)]
+				[string]
+				$path					
+			)
+	Begin {
+			$DebugPreference = "Continue"
+			if (-not $PSBoundParameters.ContainsKey('Verbose'))
+			{
+				$VerbosePreference = $PSCmdlet.GetVariableValue('VerbosePreference')
+			}
+		}	
+	Process {
+				Try {
+						$basePath = Confirm-Path -path $path
+
+				 		Get-ChildConfigFolders -path $basePath
+					
+				} catch [Exception] {
+					throw "An error occurred generating config files under path: $basePath. `r`n $_"
+				}
+				
+			}
+}
+
+function Confirm-Path {
+	Param(			
+			[Parameter(
+				Mandatory = $False )]
+				[string]$path			
+		)	
+	Import-Module "$PSScriptRoot\Get-Path.psm1"
+	Try {
+		$path = Get-Path -path $path
+		return $path
+	}
+	Catch {
+		throw $_
+	}
+	Finally {
+		Remove-Module Get-Path
+	}
+}
+
+function Get-ChildConfigFolders {
+	Param(			
+			[Parameter(
+				Mandatory = $False )]
+				[string]$path			
+		)	
+	#Convention: Get the full path to all [config] folders under the supplied path. 
+	return Get-ChildItem $path -recurse | Where-Object {$_.Name -eq '[config]'} |Sort-Object $_.FullName -Descending | foreach {$_.FullName} | Select-Object 
+}
+
+Export-ModuleMember -Function New-SolutionConfigFiles
